d58646db49bba09a5dc1743a46ad9e663d884fe3
cw <fromnowwon@gmail.com>
2024-03-17 18:17:00 +0900

    create usersReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-	return <div className="App"></div>;
+	const userList = useSelector((state) => state.users.value);
+
+	return (
+		<div className="App">
+			<div className="addUser">
+				<input type="text" placeholder="Name..." />
+				<input type="text" placeholder="Username..." />
+				<button>Add User</button>
+			</div>
+			<div className="displayUsers">
+				{userList.map((user) => {
+					return (
+						<div key={user.id}>
+							<h2>{user.name}</h2>
+							<h2>{user.username}</h2>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
